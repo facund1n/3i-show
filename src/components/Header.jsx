c0045b143017975a233b13753cc281bca5bb3f4f
facund1n <facus16s@gmail.com>
2022-11-02 03:05:00 -0300

    header "anchor" to sesion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 /* logo + social icons + búsqueda (búsqueda debe arrojar resultados al hacer enter o click en el botón de buscar, redireccionando a una página de resultados) */
 
+import { Container, Nav } from "react-bootstrap";
+
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <Container fluid>
+      <div className="d-flex justify-content-end">
+        <Nav.Link href="#action1">Iniciar Sesión</Nav.Link>
+        {"~"}
+        <Nav.Link href="#action2">Registrate</Nav.Link>
+      </div>
+    </Container>
+  );
 };
 
 export default Header;
