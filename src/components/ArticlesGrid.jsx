4d06830f8a53af394936ac7f938c77a27e2112d9
facund1n <facus16s@gmail.com>
2022-11-13 17:22:19 -0300

    key add to .map funct

--- src/components/ArticlesGrid.jsx
+++ src/components/ArticlesGrid.jsx
@@ -4,10 +4,11 @@ import Articles from "./Articles"; // Componente
 const ArticlesMapGrid = ({ data }) => {
   return (
     <Container>
+      <h1 className="f-yellow">| Destacados</h1>
       <CardGroup>
         <Row xs={2} md={4} lg={5}>
-          {data.map((article) => (
-            <Col sm={4}>
+          {data.map((article, i) => (
+            <Col sm={4} key={i}>
               <Articles article={article} />
             </Col>
           ))}
