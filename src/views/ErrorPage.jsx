56369e626e110fce96cf67ae411a886b00542885
facund1n <facus16s@gmail.com>
2022-11-13 23:31:05 -0300

    error page update

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,12 +1,28 @@
 import StickyNav from "../components/NavBars/StickyNav";
+import Footer from "../components/Footer";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
+    <Container className="px-0 d-flex flex-column min-vh-100">
       <StickyNav />
-      ErrorPage
-    </div>
+      <div className="d-flex justify-content-center m-auto">
+        <img
+          className="img-fluid"
+          src="https://tinypic.host/images/2022/11/14/404.png"
+          alt="404.png"
+          width={"520px"}
+        />
+      </div>
+      <h2 className="text-center">
+        Lo sentimos, esta página no está disponible
+      </h2>
+      <Link className="text-center f-yellow" to="/">
+        Volver a la página principal
+      </Link>
+      <Footer />
+    </Container>
   );
 };
-
 export default ErrorPage;
