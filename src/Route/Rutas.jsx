4c4d037d5cb00a8649bc86d4925f33211f8b0928
facund1n <facus16s@gmail.com>
2022-11-22 02:30:12 -0300

    login + register routes

--- src/Route/Rutas.jsx
+++ src/Route/Rutas.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ErrorPage from "../views/ErrorPage";
-/* import LogIn from "../components/LogIn"; */
-/* import Register from "../components/Register"; */
+
 import Main from "../views/Main";
 import DestacadoDetailMap from "../components/Destacados/DestacadoDetailMap";
 import NoticiaDetailMap from "../components/Noticias/NoticiaDetailMap";
 import ShowDetailMap from "../components/Shows/ShowDetailMap";
 import CommentBox from "../components/CommentBox";
+import Register from "../views/Register";
+import Login from "../views/Login";
+
 const Rutas = () => {
   return (
     <BrowserRouter>
@@ -17,6 +19,8 @@ const Rutas = () => {
         <Route path={"/noticias/:id"} element={<NoticiaDetailMap />} />
         <Route path={"/shows/:id"} element={<ShowDetailMap />} />
         <Route path={"/commentbox"} element={<CommentBox />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/register"} element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
