771400625c236a2c5070aa480c81de905bc1f93b
facund1n <facus16s@gmail.com>
2022-12-03 03:35:12 -0300

    user name clean

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -5,11 +5,12 @@ const LogOut = () => {
   const signOut = useSignOut();
 
   const getUserName = Cookies.get("_auth_state");
-  const userNoRegExp = getUserName.replace(/[^a-zA-Z 0-9.]+/g, " ");
-  const userCleaned = userNoRegExp.slice(6);
+  const userNoRegExp = getUserName.replace(/[^a-zA-Z 0-9.]+/g, "");
+  const userCleaned = userNoRegExp.slice(4);
   return (
     <div className="text-end">
-      <span>Bienvenido/a: {userCleaned}</span>
+      <span>Logedo como: </span>
+      <h3 className="f-yellow">{userCleaned}</h3>
       <div
         className="f-yellow mx-1 links"
         onClick={() => signOut().finally(window.location.reload())}
