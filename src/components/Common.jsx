542871c926a345ea688f975ae0a6041e3ac97107
facund1n <facus16s@gmail.com>
2022-12-05 02:32:04 -0300

    style add

--- src/components/Common.jsx
+++ src/components/Common.jsx
@@ -13,3 +13,17 @@ export const FormError = styled.span`
   min-height: 20px;
   font-weight: 600;
 `;
+
+export const SaveSuccess = styled.span`
+  color: #28a828;
+  font-size: 24px;
+  min-height: 100%;
+  font-weight: 500;
+`;
+
+export const SaveError = styled.span`
+  color: #f1700f;
+  font-size: 24px;
+  min-height: 100%;
+  font-weight: 500;
+`;
