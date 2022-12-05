367d30db3e06a3e79818599912d7de56fdfcad4d
facund1n <facus16s@gmail.com>
2022-12-05 02:32:39 -0300

    user name tunning

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -1,4 +1,5 @@
 import { useSignOut } from "react-auth-kit";
+import { Link } from "react-router-dom";
 import Cookies from "js-cookie";
 
 const LogOut = () => {
@@ -8,9 +9,14 @@ const LogOut = () => {
   const userNoRegExp = getUserName.replace(/[^a-zA-Z 0-9.]+/g, "");
   const userCleaned = userNoRegExp.slice(4);
   return (
-    <div className="text-end">
+    <div className="text-end justify-content-end">
       <span>Logedo como: </span>
-      <h3 className="f-yellow">{userCleaned}</h3>
+      <br />
+      <Link to={`/users/${userCleaned}`}>
+        <h3 className="f-yellow btn btn-outline-warning btn-lg my-2">
+          {userCleaned}
+        </h3>
+      </Link>
       <div
         className="f-yellow mx-1 links"
         onClick={() => signOut().finally(window.location.reload())}
