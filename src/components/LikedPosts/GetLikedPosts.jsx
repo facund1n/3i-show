78cb832dd0766ae191d39f87de1311a23aa678fc
facund1n <facus16s@gmail.com>
2022-12-11 03:31:33 -0300

    Liked post component add

--- src/components/LikedPosts/GetLikedPosts.jsx
+++ src/components/LikedPosts/GetLikedPosts.jsx
@@ -1,5 +1,25 @@
-const GetLikedPosts = ({ userLogged }) => {
-  return <div>LIKED POSTS</div>;
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import LikedPostGrid from "./LikedPostGrid";
+import Loader from "../Loader";
+
+const GetLikedPosts = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); //loader
+  const params = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/users/${params.id}/liked/`)
+      .then((res) => res.json())
+      .then((json) => setData(json))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <>
+      {isLoading ? <Loader /> : data.map((l) => <LikedPostGrid l={l.liked} />)}
+    </>
+  );
 };
 
 export default GetLikedPosts;
