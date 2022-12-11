5e05108f7982d2380eaa50e865a4739098716b1d
facund1n <facus16s@gmail.com>
2022-12-11 03:29:52 -0300

    user panel  update

---
+++ src/components/UserPanel/UserPanel.jsx
@@ -0,0 +1,21 @@
+import StickyNav from "../NavBars/StickyNav";
+import Footer from "../Footer";
+
+import { Container } from "react-bootstrap";
+
+import LikedButton from "./LikedButton";
+import SavedButton from "./SavedButton";
+
+const UserPanel = ({ userLogged }) => {
+  return (
+    <>
+      <Container className="px-0 d-flex flex-column min-vh-100">
+        <StickyNav />
+        <SavedButton userLogged={userLogged} />
+        <LikedButton userLogged={userLogged} />
+      </Container>{" "}
+      <Footer />
+    </>
+  );
+};
+export default UserPanel;
