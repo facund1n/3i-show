061cd60628aeae0b38503a44a432844e668f4083
facund1n <facus16s@gmail.com>
2022-12-11 23:04:13 -0300

    add link a buscador

--- src/components/NavBars/StickyNav.jsx
+++ src/components/NavBars/StickyNav.jsx
@@ -1,9 +1,16 @@
-import { Container, Form, Nav, Navbar } from "react-bootstrap";
+import { Container, Form, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import SearchResults from "../Search/SearchResults";
 import Social from "../Social";
 
 function StickyNav() {
   return (
-    <Navbar className="b-grey" variant="dark" expand="lg" sticky="top">
+    <Navbar
+      /* className="b-grey" */
+      className="b-grey"
+      expand="lg"
+      sticky="top"
+      variant="dark"
+    >
       <Container>
         <Navbar.Brand href="/">
           <img
@@ -16,13 +23,34 @@ function StickyNav() {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="ms-auto align-items-center">
-            <Nav.Link href="/">HOME</Nav.Link>
-            <Nav.Link href="/#destacadosIndex">DESTACADOS</Nav.Link>
-            <Nav.Link href="/#noticiasIndex">NOTICIAS</Nav.Link>
-            <Nav.Link href="/#fotosIndex">FOTOS</Nav.Link>
-            <Nav.Link href="/#showsIndex">SHOWS</Nav.Link>
-            <Nav.Link href="/contacto">CONTACTO</Nav.Link>
-            <Social /> {/* redes sociales */}
+            <Nav.Link href="/" className="f-yellow">
+              HOME
+            </Nav.Link>
+            <NavDropdown
+              title="CATEGORÍAS"
+              id="basic-nav-dropdown"
+              className="b-grey"
+            >
+              <NavDropdown.Item href="#action/3.1">
+                <Nav.Link href="/#destacadosIndex">DESTACADOS</Nav.Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                <Nav.Link href="/#noticiasIndex">NOTICIAS</Nav.Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                <Nav.Link href="/#fotosIndex">FOTOS</Nav.Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                <Nav.Link href="/#showsIndex">SHOWS</Nav.Link>
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="/contacto" className="f-yellow">
+              CONTACTO
+            </Nav.Link>{" "}
+            <Nav.Link href="/busqueda" className="f-yellow">
+              Buscar
+            </Nav.Link>
+            <Social />
           </Nav>
           <Form className="d-flex"></Form>
         </Navbar.Collapse>
