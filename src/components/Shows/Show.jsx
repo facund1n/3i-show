64f0824f8dfd93f369b76ae2275a09355d871d0c
facund1n <facus16s@gmail.com>
2022-12-13 21:43:41 -0300

    prop update

--- src/components/Shows/Show.jsx
+++ src/components/Shows/Show.jsx
@@ -1,29 +1,39 @@
 import { Card } from "react-bootstrap";
-/* import { Link } from "react-router-dom"; */
+import { Link } from "react-router-dom";
 
 // 3
 
-const Show = ({ show }) => {
+const Show = ({ data }) => {
   return (
     <>
-      <Card bg="dark" border="black" className="mb-2" style={{ height: "95%" }}>
-        <Card.Img src={show.image} alt={show.alt} style={{ height: "50%" }} />
-        <Card.Body>
-          <Card.Text>
-            {show.title}
-            <br />
-            {show.subtitle}
-          </Card.Text>
-          <hr />
-          <Card.Text>
-            <span>Tickets en:</span>
-            <br />
-            <a href={show.ticketsUrl} className="f-yellow">
-              {show.ticketsUrl}
-            </a>
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <Link
+        to={`/articulos/shows/${data._id}`}
+        style={{ textDecoration: "inherit" }}
+      >
+        <Card
+          bg="dark"
+          border="black"
+          className="mb-2 bg-dark links"
+          style={{ height: "95%" }}
+        >
+          <Card.Img src={data.image} alt={data.alt} style={{ height: "50%" }} />
+          <Card.Body>
+            <Card.Text>
+              {data.title}
+              <br />
+              {data.subtitle}
+            </Card.Text>
+            <hr />
+            <Card.Text>
+              <span>Tickets en:</span>
+              <br />
+              <a href={data.ticketsUrl} className="f-yellow">
+                {data.ticketsUrl}
+              </a>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Link>
     </>
   );
 };
