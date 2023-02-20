42abc958bdd02726350d84e87ea3b169380c0382
facund1n <facus16s@gmail.com>
2023-02-20 17:31:56 -0300

    New Post Button

---
+++ src/components/CRUD/NewPostButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const DeletePostButton = () => {
+  return <Button variant="sucsess">NUEVO</Button>;
+};
+
+export default DeletePostButton;
