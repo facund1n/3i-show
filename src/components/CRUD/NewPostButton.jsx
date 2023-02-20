730c0b36459af00f1b72cf24e17b9fb0e4760b0b
facund1n <facus16s@gmail.com>
2023-02-20 17:34:41 -0300

    button fix

--- src/components/CRUD/NewPostButton.jsx
+++ src/components/CRUD/NewPostButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 const DeletePostButton = () => {
-  return <Button variant="sucsess">NUEVO</Button>;
+  return <Button variant="success">NUEVO</Button>;
 };
 
 export default DeletePostButton;
