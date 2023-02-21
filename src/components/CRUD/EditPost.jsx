f72edd039354f2b62d2e2f4efc33715aae89b226
facund1n <facus16s@gmail.com>
2023-02-21 19:54:31 -0300

    edit post select fix

--- src/components/CRUD/EditPost.jsx
+++ src/components/CRUD/EditPost.jsx
@@ -148,6 +148,8 @@ export default function EditPost({ data }) {
         className="mb-2"
         id="category"
         name="category"
+        onChange={formik.handleChange}
+        value={formik.values.category}
       >
         <option value="destacados" className="f-black">
           Destacados
